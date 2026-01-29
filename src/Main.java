69a68d548ac5ce2b3210349c132ccd2eb0b87c30
MauroGutMB <mgmbarros2006@gmail.com>
2026-01-29 20:33:16 -0300

    Gun image in window

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import core.*;
 import game.*;
 import javax.swing.*;
 import java.awt.*;
+import javax.imageio.ImageIO;
+import java.io.IOException;
 
 /**
  * Classe principal do jogo
@@ -15,6 +17,14 @@ public class Main {
             frame.setSize(Settings.GAME_WIDTH, Settings.GAME_HEIGHT);
             frame.setLocationRelativeTo(null);
             frame.setResizable(false);
+            
+            // Define o ícone da janela
+            try {
+                Image icon = ImageIO.read(Main.class.getResourceAsStream("/assets/revolver.png"));
+                frame.setIconImage(icon);
+            } catch (IOException e) {
+                System.err.println("Erro ao carregar ícone da janela: " + e.getMessage());
+            }
             
             // Cria o gerenciador de telas
             ScreenManager screenManager = new ScreenManager(frame);
